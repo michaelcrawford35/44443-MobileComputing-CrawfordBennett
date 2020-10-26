30848902447fd77a716364cedc40c33268142bee
Michael Crawford <mcrawbeast35@gmail.com>
2020-10-26 12:21:50 -0500

    Initial Commit - Created project

---
+++ FinalProject-AdventurerTextSim/FinalProject-AdventurerTextSim/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FinalProject-AdventurerTextSim
+//
+//  Created by student on 10/26/20.
+//  Copyright © 2020 Northwest. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
