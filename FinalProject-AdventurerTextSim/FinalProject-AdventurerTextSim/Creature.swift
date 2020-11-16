d04003cad28b436a5cd1dca0070e411b5263fa9e
Seth Bennett <S530995@nwmissouri.edu>
2020-11-16 13:42:35 -0600

    creature done starting map

--- FinalProject-AdventurerTextSim/FinalProject-AdventurerTextSim/Creature.swift
+++ FinalProject-AdventurerTextSim/FinalProject-AdventurerTextSim/Creature.swift
@@ -10,4 +10,89 @@ import Foundation
 
 class Creature{
     
+    private var creatureName:String;
+    private var creatureHealth: Int;
+    private var creatureDamage: Int;
+    private var creatureDrops: [Int];
+    private var isAlive: Bool;
+    private var creatureMaxHealth:Int;
+
+    private init(name: String, baseHealth:Int, baseDamage:Int, drops:[Int]){
+        creatureName = name;
+        creatureHealth = baseHealth;
+        creatureMaxHealth = baseHealth
+        creatureDamage = baseDamage;
+        creatureDrops = drops;
+        isAlive = true;
+    }
+    
+    func modifyCreature(playerLevel: Int){
+        for _ in 1...playerLevel{
+            creatureHealth += creatureHealth/10;
+            creatureMaxHealth = creatureHealth;
+            creatureDamage += creatureDamage/20;
+        }
+        
+    }
+    func getDamage()->Int{
+        return creatureDamage;
+    }
+    
+    func damageCreature(num: Int){
+        creatureHealth -= num;
+        if creatureHealth <= 0{
+            isAlive = false;
+        }
+    }
+    func healCreature(num: Int){
+        creatureHealth += num;
+        if creatureHealth > creatureMaxHealth{
+            creatureHealth = creatureMaxHealth;
+        }
+    }
+    func lifeCheck()->Bool{
+        return isAlive;
+    }
+    func getName()->String{
+        return creatureName;
+    }
+    
+
+}
+
+struct CreatureMaker {
+    private static var _cMaker:CreatureMaker!
+    
+    static var cMaker:CreatureMaker{
+        if _cMaker==nil {
+            _cMaker = CreatureMaker();
+            
+        }
+        // add creatures to array here
+        
+        
+        
+        return _cMaker
+    }
+    
+    private var creatureArray:[Creature]
+    
+    private init(){
+        creatureArray = [];
+    }
+    
+    mutating func addCreature(it: Creature){
+        creatureArray.append(it);
+    }
+    func getCreature(_ name: String)-> Creature{
+        
+        for crea in creatureArray{
+            var creaName = crea.getName();
+            if creaName == name{
+                return crea
+            }
+        }
+        return creatureArray[0];
+    }
+    
 }
---
+++ FinalProject-AdventurerTextSim/FinalProject-AdventurerTextSim/Map.swift
@@ -0,0 +1,15 @@
+//
+//  Map.swift
+//  FinalProject-AdventurerTextSim
+//
+//  Created by Student on 11/16/20.
+//  Copyright © 2020 Northwest. All rights reserved.
+//
+
+import Foundation
+
+struct Map{
+    
+    
+    
+}
